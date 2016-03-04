12f29a90012a109b27a845e7588ba92778e787bb
Ali Sinan Köksal <alisinan@gmail.com>
2016-03-04 15:17:35 -0800

    fine-tuning of linear equation generation

--- scripts/R/simulation.R
+++ scripts/R/simulation.R
@@ -1,23 +1,32 @@
 options(warn = 1)
 library(polynom)
 
+args = commandArgs(trailingOnly = TRUE)
+outputFolder = args[[1]]
+
+# create subdir for cluster output
+dir.create(outputFolder)
+
 # pick measurement times
 minT = 0
 maxT = 10
 stepT = 1
 
+speedCoefStdDev = 0.5
+measurementNoiseStdDev = 5
+
 # pick sensible parameters for instantiating polynomials
 generatePolynomial <- function(degree) {
   zeros = sample(minT:maxT, degree)
-  a = sample(seq(from = -2, to = 2, by = 0.1), 1)
-  b = sample(seq(from = 0, to = 10, by = 0.1), 1)
+  a = sample(seq(from = -10, to = 10, by = 0.1), 1)
+  b = sample(seq(from = -50, to = 50, by = 0.1), 1)
   return(polynomial(coef = c(b, a)))
 }
 
 nbPolynomials = 11
 degree = 1
 generateValuesWithNoise <- function(ps) {
-  nbCellsPerMeasurement = 1000
+  nbCellsPerMeasurement = 100
   measurementTimes = seq(from = minT, to = maxT, by = stepT)
 
   originalData = matrix(ncol = length(ps) + 1, nrow = 0)
@@ -26,12 +35,12 @@ generateValuesWithNoise <- function(ps) {
   for (t in measurementTimes) {
     for (c in 1:nbCellsPerMeasurement) {
       # give this cell a stochastic time value
-      speedCoef = rnorm(1, mean = 1, sd = 1)
-      actualTime = max(speedCoef * t, 0)
+      speedCoef = rnorm(1, mean = 0, sd = speedCoefStdDev)
+      actualTime = max(0, speedCoef * t)
       actualValues = lapply(ps, function(p) predict(p, actualTime))
 
       # add measurement noise
-      noisyValues  = lapply(actualValues, function(v) v + rnorm(1, mean = 0, sd = 1))
+      noisyValues  = lapply(actualValues, function(v) v + rnorm(1, mean = 0, sd = measurementNoiseStdDev))
 
       originalData = rbind(originalData, c(actualTime, actualValues))
       observedData = rbind(observedData, c(t, noisyValues))
@@ -46,14 +55,14 @@ generateValuesWithNoise <- function(ps) {
 
   write.table(
               originalData, 
-              file = "original.tsv", 
+              file = paste(outputFolder, "/original.tsv", sep = ""),
               sep = "\t", 
               row.names = FALSE, 
               col.names = FALSE
               )
   write.table(
               observedData, 
-              file = "observed.tsv", 
+              file = paste(outputFolder, "/observed.tsv", sep = ""),
               sep = "\t", 
               row.names = FALSE, 
               col.names = FALSE
