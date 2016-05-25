498e96540d97e542394d37ab17c2761625ef3090
Ali Sinan Köksal <alisinan@gmail.com>
2016-05-25 16:06:15 -0700

    tentative alternative cell reordering method

---
+++ src/main/scala/karme/CellReordering.scala
@@ -0,0 +1,65 @@
+package karme
+
+object CellReordering {
+  def computePseudotimes(exp: Experiment): Experiment = {
+    val meanValuesPerTime: Map[Double, IndexedSeq[Double]] = computeMeanCells(exp)
+
+    // place each cell between two mean vectors.
+    // find two closest mean cells
+    // place between the two
+    // pseudotime will be the weighted average of mean cell time stamps
+    // question: can we detect if cell is not on the segment between the two?
+
+    val msWithPt = exp.measurements.map { m =>
+      val pt = computeCellPseudotime(m, meanValuesPerTime)
+      m.copy(pseudotime = pt)
+    }
+
+    exp.copy(measurements = msWithPt)
+  }
+
+  private def computeMeanCells(exp: Experiment): Map[Double, IndexedSeq[Double]] = {
+    val cellsBySamplingTime = exp.measurements.groupBy(_.time)
+    val means = cellsBySamplingTime.map{ case (t, ms) =>
+      t -> meanValues(ms)
+    }
+    means
+  }
+
+  private def meanValues(ms: Seq[CellMeasurement]): IndexedSeq[Double] = {
+    assert(!ms.isEmpty)
+    val n = ms.head.values.size
+    val meanValues = (0 until n) map { i =>
+      val ivs = ms.map(_.values(i))
+      Util.mean(ivs)
+    }
+    meanValues
+  }
+
+  private def computeCellPseudotime(
+    m: CellMeasurement, 
+    means: Map[Double, IndexedSeq[Double]]
+  ): Double = {
+    val allowedSamplingTimes = means.keySet.toList.sortBy(samplingTime => math.abs(samplingTime - m.time)).take(3)
+    val distances = allowedSamplingTimes map { t =>
+      val mvs = means(t)
+      t -> distance(m.values, mvs)
+    }
+
+    val sortedDistances = distances.toList.sortBy(_._2)
+    val List((t1, d1), (t2, d2)) = sortedDistances take 2
+
+    // place cell between t1 and t2
+    val scaleF = RangeScaling.scalingFun(0, d1 + d2, t1, t2)
+    scaleF(d1)
+  }
+
+  private def distance(vs1: IndexedSeq[Double], vs2: IndexedSeq[Double]): Double = {
+    val squares = vs1.zip(vs2).map{ case (v1, v2) => 
+      val diff = v1 - v2
+      diff * diff
+    }
+    val squareSum = squares.sum
+    math.sqrt(squareSum)
+  }
+}
