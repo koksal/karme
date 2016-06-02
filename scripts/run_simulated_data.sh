5b3483d80588d7bbaee959a1665cc48596e9a9c5
Ali Sinan Köksal <alisinan@gmail.com>
2016-06-02 11:21:17 -0700

    adding label argument to simulation running script

--- scripts/run_simulated_data.sh
+++ scripts/run_simulated_data.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+LABEL="simulated-data
+"
+if [ "$1" != "" ]; then
+  LABEL=$1
+fi
+
 ./scripts/run_experiment_with_default_args.sh \
   data/simulated/gnw-linear-0.20-sde-10k-runs/single-cell.csv \
   data/simulated/names/linear.txt \
-  single-cell-sim
+  $LABEL
