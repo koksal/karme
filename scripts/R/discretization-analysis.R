e5571f868cc4483ca57691c3402623a013e83130
Ali Sinan Köksal <alisinan@gmail.com>
2016-10-11 17:06:21 -0700

    R script to compare discretization results across tools

---
+++ scripts/R/discretization-analysis.R
@@ -0,0 +1,30 @@
+library(ggplot2)
+library(Ckmeans.1d.dp)
+library(diptest)
+suppressPackageStartupMessages(library(mclust))
+
+data = read.csv("~/Desktop/OEcounts.csv")
+# drop first column, the cell name
+names = tail(names(data), -1)
+
+cat(paste("gene", "ckmeans # clusters", "dip test statistic", "dip test p-value", "mclust # clusters", sep = ","))
+cat("\n")
+
+for (n in names) {
+  vs = data[[n]]
+
+  # plot = qplot(vs, data = NULL, geom = "histogram")
+  # ggsave(plot, file = paste("histograms/", n, "-log.pdf", sep = ""))
+
+  ckmeansResult = suppressWarnings(Ckmeans.1d.dp(vs, k = c(1, 2)))
+  min = min(ckmeansResult$cluster)
+  max = max(ckmeansResult$cluster)
+  nbLevels = max - min + 1
+
+  dipTestResult = dip.test(vs)
+
+  mclustResult = Mclust(vs, G=1:2)
+
+  cat(paste(n, nbLevels, dipTestResult$statistic, dipTestResult$p.value, mclustResult$G, sep = ","))
+  cat("\n")
+}
