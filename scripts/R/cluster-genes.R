db69f105e957e5b22e0311f28e7b4b7d9443ecd3
Ali Sinan Köksal <alisinan@gmail.com>
2017-01-12 18:19:16 -0800

    back to ward hierarchical clustering

--- scripts/R/cluster-genes.R
+++ scripts/R/cluster-genes.R
@@ -12,6 +12,6 @@ valuesPerGene = t(cellValues)
 geneIDs = rownames(valuesPerGene)
 
 res<-NbClust(valuesPerGene, diss=NULL, distance = "euclidean", min.nc=2, max.nc=50,
-             method = "kmeans", index = "all")
+             method = "ward.D2", index = "all")
 
 print(res)
