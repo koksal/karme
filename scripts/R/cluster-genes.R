c61f2c8e683aa11e8ff0118ece373a36c6add869
Ali Sinan Köksal <alisinan@gmail.com>
2017-01-12 17:33:06 -0800

    R script for clustering genes

---
+++ scripts/R/cluster-genes.R
@@ -0,0 +1,17 @@
+options(warn = 1)
+library(NbClust)
+
+args = commandArgs(trailingOnly = TRUE)
+inputFile = args[[1]]
+
+d = read.csv(inputFile)
+
+cellIDs = d[, "id"]
+cellValues = d[, -1]
+valuesPerGene = t(cellValues)
+geneIDs = rownames(valuesPerGene)
+
+res<-NbClust(valuesPerGene, diss=NULL, distance = "euclidean", min.nc=2, max.nc=50,
+             method = "ward.D2", index = "kl")
+
+print(res)
