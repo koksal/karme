b7078cfbc826f78b15a93ded1dd1e4c5d369dd78
Ali Sinan Köksal <alisinan@gmail.com>
2017-03-07 19:47:27 -0800

    fix parallel sbt usage

--- scripts/run-range.sh
+++ scripts/run-range.sh
@@ -1,11 +1,11 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
 set -o xtrace
 
 function run_with_args() {
   folder="run_"`echo $@ | sed s'/[\ \/-]/_/g'`
   echo $folder
-  scripts/run-default.sh $folder "$@" &
+  scripts/run-default.sh $folder "$@"
 }
 
 # export function so parallel can access it
@@ -13,7 +13,7 @@ export -f run_with_args
 
 # filtering genes by name
 declare -A FILTER_NAMES_ARGS
-FILTER_NAMES_ARGS["none"]=""
+# FILTER_NAMES_ARGS["none"]=""
 FILTER_NAMES_ARGS["markers"]="--names data/names/markers.txt"
 FILTER_NAMES_ARGS["human-tf"]="--names data/names/human-tf.txt"
 FILTER_NAMES_ARGS["animaltfdb"]="--names data/names/animaltfdb-mus-musculus.txt"
@@ -33,6 +33,7 @@ ACTIVITY_FILTER_ARGS["10-percent"]="--activity-ratio 0.1"
 ACTIVITY_FILTER_ARGS["20-percent"]="--activity-ratio 0.2"
 ACTIVITY_FILTER_ARGS["30-percent"]="--activity-ratio 0.3"
 
-parallel --files run_with_args \
-  ::: "--bogus argument"
-  # ::: "${FILTER_NAMES_ARGS[@]}"
+SHELL="/bin/bash" parallel run_with_args \
+  ::: "${FILTER_NAMES_ARGS[@]}" \
+  ::: "${DATA_TRANSFORM_ARGS[@]}" \
+  ::: "${ACTIVITY_FILTER_ARGS[@]}"
