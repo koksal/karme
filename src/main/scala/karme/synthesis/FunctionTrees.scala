7452f934b69c188ec7df1536ddf52f4038afa75d
Ali Sinan Köksal <alisinan@gmail.com>
2017-03-16 11:16:45 -0700

    extract encoding mapping

--- src/main/scala/karme/synthesis/FunctionTrees.scala
+++ src/main/scala/karme/synthesis/FunctionTrees.scala
@@ -19,37 +19,6 @@ object FunctionTrees {
     case FunNot(e) => !eval(e, in)
   }
 
-  class EncodingMapping(variableNames: Seq[String]) {
-    sealed trait EncodingMappingValue
-    case object AndEncodingValue extends EncodingMappingValue
-    case object OrEncodingValue extends EncodingMappingValue
-    case object NotEncodingValue extends EncodingMappingValue
-    case object IgnoreEncodingValue extends EncodingMappingValue
-
-    private val nonVariableEncodingValues = List(AndEncodingValue,
-      OrEncodingValue, NotEncodingValue, IgnoreEncodingValue)
-
-    private def encodingValueConstant(emv: EncodingMappingValue): Int = {
-      assert(nonVariableEncodingValues.contains(emv))
-      nonVariableEncodingValues.indexOf(emv)
-    }
-
-    val AND_NODE = IntLiteral(encodingValueConstant(AndEncodingValue))
-    val OR_NODE = IntLiteral(encodingValueConstant(OrEncodingValue))
-    val NOT_NODE = IntLiteral(encodingValueConstant(NotEncodingValue))
-    val IGNORE_NODE = IntLiteral(encodingValueConstant(IgnoreEncodingValue))
-
-    def VAR_NODE(name: String): Expr = {
-      IntLiteral(nonVariableEncodingValues.size + variableNames.indexOf(name))
-    }
-
-    val VAR_NODE_RANGE: Seq[Expr] = variableNames map VAR_NODE
-
-    def VAR_NAME(modelValue: Int): String = {
-      variableNames(modelValue - nonVariableEncodingValues.size)
-    }
-  }
-
   abstract class SymFunExpr {
     val possibleVars: Set[String]
     val orderedVariableOptions: List[String] = possibleVars.toList.sorted
