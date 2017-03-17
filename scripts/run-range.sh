e19af188f6a5c7edf4104afd25ed4d1af0cafeeb
Ali Sinan Köksal <alisinan@gmail.com>
2017-03-17 13:45:06 -0700

    range evaluation parameter bounds tweak

--- scripts/run-range.sh
+++ scripts/run-range.sh
@@ -6,13 +6,13 @@ function run_with_args() {
   folder="log/parallel_runs/run_"`echo $@ | sed s'/[\ \/-]/_/g'`
   echo $folder
   scripts/run-default.sh $folder \
-    --annotations data/names/markers.txt \
+    # --annotations data/names/markers.txt \
+    --synthesis \
+    --simulation \
     --uncertainty 0.0 \
-    --cluster \
-    --minClust 2 \
-    --maxClust 30 \
-    --curves \
+    --max-expr-depth 5 \
     --graphs \
+    --curves \
     "$@"
 }
 
@@ -23,10 +23,10 @@ export -f run_with_args
 declare -A FILTER_NAMES_ARGS
 # FILTER_NAMES_ARGS["none"]=""
 # FILTER_NAMES_ARGS["markers"]="--names data/names/markers.txt"
-# FILTER_NAMES_ARGS["human-tf"]="--names data/names/human-tf.txt"
+FILTER_NAMES_ARGS["human-tf"]="--names data/names/human-tf.txt"
 # FILTER_NAMES_ARGS["animaltfdb"]="--names data/names/animaltfdb-mus-musculus.txt"
-FILTER_NAMES_ARGS["markers-and-human-tf"]="--names data/names/markers.txt,data/names/human-tf.txt"
-FILTER_NAMES_ARGS["markers-and-animaltfdb"]="--names data/names/markers.txt,data/names/animaltfdb-mus-musculus.txt"
+# FILTER_NAMES_ARGS["markers-and-human-tf"]="--names data/names/markers.txt,data/names/human-tf.txt"
+# FILTER_NAMES_ARGS["markers-and-animaltfdb"]="--names data/names/markers.txt,data/names/animaltfdb-mus-musculus.txt"
 
 # data transformation
 declare -A DATA_TRANSFORM_ARGS
@@ -44,12 +44,12 @@ ACTIVITY_FILTER_ARGS["20-percent"]="--activity-ratio 0.2"
 # force include annotations in data
 declare -A FORCE_ANNOTATION_ARGS
 FORCE_ANNOTATION_ARGS["no"]=""
-FORCE_ANNOTATION_ARGS["yes"]="--force-annotations"
+# FORCE_ANNOTATION_ARGS["yes"]="--force-annotations"
 
 # first discretization
 declare -A FIRST_DISCRETIZATION_ARGS
 FIRST_DISCRETIZATION_ARGS["kmeans"]="--first-discretization kmeans"
-FIRST_DISCRETIZATION_ARGS["mclust"]="--first-discretization mclust"
+# FIRST_DISCRETIZATION_ARGS["mclust"]="--first-discretization mclust"
 
 # smoothing
 declare -A SMOOTHING_RADIUS_ARGS
@@ -58,10 +58,20 @@ declare -A SMOOTHING_RADIUS_ARGS
 SMOOTHING_RADIUS_ARGS["20"]="--smoothing-radius 20"
 # SMOOTHING_RADIUS_ARGS["30"]="--smoothing-radius 30"
 
+# clustering
+declare -A CLUSTERING_ARGS
+CLUSTERING_ARGS["10"]="--cluster --minClust 10 --maxClust 10"
+CLUSTERING_ARGS["12"]="--cluster --minClust 12 --maxClust 12"
+CLUSTERING_ARGS["14"]="--cluster --minClust 14 --maxClust 14"
+CLUSTERING_ARGS["16"]="--cluster --minClust 16 --maxClust 16"
+CLUSTERING_ARGS["18"]="--cluster --minClust 18 --maxClust 18"
+CLUSTERING_ARGS["20"]="--cluster --minClust 20 --maxClust 20"
+
 SHELL="/bin/bash" parallel --jobs 4 --delay 30 run_with_args \
   ::: "${FILTER_NAMES_ARGS[@]}" \
   ::: "${DATA_TRANSFORM_ARGS[@]}" \
   ::: "${ACTIVITY_FILTER_ARGS[@]}" \
   ::: "${FORCE_ANNOTATION_ARGS[@]}" \
   ::: "${FIRST_DISCRETIZATION_ARGS[@]}" \
-  ::: "${SMOOTHING_RADIUS_ARGS[@]}"
+  ::: "${SMOOTHING_RADIUS_ARGS[@]}" \
+  ::: "${CLUSTERING_ARGS[@]}"
