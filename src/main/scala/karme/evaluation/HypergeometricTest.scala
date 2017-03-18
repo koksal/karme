b00dd86ea9543ea2fbeb175c1ff59f731ea185f2
Ali Sinan Köksal <alisinan@gmail.com>
2017-03-17 17:02:07 -0700

    hypergeometric test and its test

---
+++ src/main/scala/karme/evaluation/HypergeometricTest.scala
@@ -0,0 +1,21 @@
+package karme.evaluation
+
+import karme.external.AbstractRInterface
+import org.ddahl.rscala.RClient
+
+class HypergeometricTest(
+  nbSamples: Int,
+  nbSampleSuccesses: Int,
+  nbTotalSuccesses: Int,
+  nbTotalFailures: Int
+) extends AbstractRInterface[Double] {
+
+  def process(R: RClient): Double = {
+    call(R)("phyper", "res", "q" -> (nbSampleSuccesses - 1),
+      "m" -> nbTotalSuccesses, "n" -> nbTotalFailures, "k" -> nbSamples,
+      "lower.tail" -> "FALSE")
+
+    R.evalD0("res")
+  }
+
+}
---
+++ src/test/scala/karme/evaluation/HypergeometricTestTest.scala
@@ -0,0 +1,17 @@
+package karme.evaluation
+
+import org.scalactic.TolerantNumerics
+import org.scalatest.FunSuite
+
+class HypergeometricTestTest extends FunSuite {
+  test("hypergeometric test") {
+    val resultPValue = new HypergeometricTest(nbSamples = 10,
+      nbSampleSuccesses = 5, nbTotalSuccesses = 100,
+      nbTotalFailures = 100).run()
+
+    val expected = 0.6262351
+
+    val equality = TolerantNumerics.tolerantDoubleEquality(0.0001)
+    assert(equality.areEqual(expected, resultPValue))
+  }
+}
