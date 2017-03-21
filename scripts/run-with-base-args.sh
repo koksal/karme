bec59dd563225106f489b72ea5adf748869e5211
Ali Sinan Köksal <alisinan@gmail.com>
2017-03-20 17:34:31 -0700

    running scripts

--- scripts/run-with-base-args.sh
+++ scripts/run-with-base-args.sh
@@ -7,7 +7,7 @@ shift
 mkdir -p $OUTFOLDER
 
 sbt "run-main karme.Main \
-  --contexp data/measurements/continuous-experiment.csv \
+  --continuous-experiment data/measurements/continuous-experiment.csv \
   --cell-clusters data/cell-clustering/clustering-renamed.csv \
-  --trajectory data/trajectories/curve1-pseudotime.csv,data/trajectories/curve2-pseudotime.csv,data/trajectories/curve3-pseudotime.csv \
+  --trajectories data/trajectories/curve1-pseudotime.csv,data/trajectories/curve2-pseudotime.csv,data/trajectories/curve3-pseudotime.csv \
   --outfolder $OUTFOLDER $*" | tee $LOGFILE
---
+++ scripts/run-with-default-args.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OUTFOLDER=$1
+
+./scripts/run-with-base-args.sh $OUTFOLDER \
+  --names data/names/human-tf.txt \
+  --annotations data/names/markers.txt \
+  --synthesis \
+  --pseudolog-factor 2 \
+  --boolean-normalization mclust \
+  --cell-activity-threshold 0.2 \
+  --uncertainty-threshold 0.4 \
+  --smoothing-radius 20 \
+  --max-hamming 1 \
+  --cluster \
+  --min-clusters 10 \
+  --max-clusters 20 \
+  --max-expr-depth 5
