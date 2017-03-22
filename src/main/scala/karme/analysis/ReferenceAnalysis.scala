476fa9ad1cc15e64564727354e1e09ddbb06a168
Ali Sinan Köksal <alisinan@gmail.com>
2017-03-22 13:59:15 -0700

    dding all libraries to script + reference-cluster analysis sketch

---
+++ src/main/scala/karme/analysis/ReferenceAnalysis.scala
@@ -0,0 +1,11 @@
+package karme.analysis
+
+object ReferenceAnalysis {
+
+  def main(args: Array[String]): Unit = {
+    // How many predictions are within cluster?
+    // How many predictions for each cluster pair?
+    // Should we look into cluster sizes and the impact on # predictions?
+  }
+
+}
