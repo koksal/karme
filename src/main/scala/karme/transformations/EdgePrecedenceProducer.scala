f3312e2444507c67b32ddb3b6c492a4489a1724c
Ali Sinan Köksal <alisinan@gmail.com>
2017-05-21 16:34:25 -0700

    use shortest paths to produce all precedence data

--- src/main/scala/karme/transformations/EdgePrecedenceProducer.scala
+++ src/main/scala/karme/transformations/EdgePrecedenceProducer.scala
@@ -1,5 +1,7 @@
 package karme.transformations
 
+import karme.graphs.Graphs.UnlabeledEdge
+import karme.graphs.StateGraphs.UndirectedStateGraphOps
 import karme.graphs.StateGraphs.{DirectedBooleanStateGraph, StateGraphVertex}
 
 case class EdgePrecedence(source: String, target: String, distance: Int)
@@ -13,10 +15,25 @@ class EdgePrecedenceProducer {
   def computePrecedenceFromNode(
     g: DirectedBooleanStateGraph, v: StateGraphVertex
   ): Seq[EdgePrecedence] = {
-    // compute shortest path to all other nodes, reconstruct precedence
-    // information
-    // do a BFS
-    ???
+    val shortestPaths = g.shortestPaths(v)
+    val nonTrivialPaths = shortestPaths.filter(_.size > 1)
+
+    nonTrivialPaths.toSeq flatMap precedenceFromPath
+  }
+
+  def precedenceFromPath(p: Seq[StateGraphVertex]): Seq[EdgePrecedence] = {
+    require(p.size >= 2)
+
+    val distance = p.size - 1
+    val firstEdge = UnlabeledEdge(p(0), p(1))
+    val lastEdge = UnlabeledEdge(p(p.size - 2), p(p.size - 1))
+
+    for {
+      l1 <- UndirectedStateGraphOps.edgeLabels(firstEdge)
+      l2 <- UndirectedStateGraphOps.edgeLabels(lastEdge)
+    } yield {
+      EdgePrecedence(l1, l2, distance)
+    }
   }
 
 }
