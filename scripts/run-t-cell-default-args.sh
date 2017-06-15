3583d6603f7ed93dbee54d974fb89166950c0c02
Ali Sinan Köksal <alisinan@gmail.com>
2017-06-15 14:16:57 -0700

    fix cluster expansion to take into account empty clusters (#3)

--- scripts/run-t-cell-default-args.sh
+++ scripts/run-t-cell-default-args.sh
@@ -3,7 +3,7 @@
 OUTFOLDER=$1
 
 ./scripts/run-t-cell-base-args.sh $OUTFOLDER \
-  --pseudolog-factor 10 \
+  --pseudolog-factor 2 \
   --boolean-normalization mclust \
   --cell-activity-threshold 0.2 \
   --uncertainty-threshold 1 \
