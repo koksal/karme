f891d3ed0bedd8883a8e2ab3ca3972489c16ecc8
Ali Sinan Köksal <alisinan@gmail.com>
2017-09-18 20:44:52 -0700

    finish linear graph topology generation

--- src/main/scala/karme/evaluation/synthetic/topology/LinearNetworkGeneration.scala
+++ src/main/scala/karme/evaluation/synthetic/topology/LinearNetworkGeneration.scala
@@ -1,10 +1,25 @@
 package karme.evaluation.synthetic.topology
-import karme.evaluation.synthetic.topology.NetworkTopologyGraphs.NetworkTopologyGraph
+import karme.evaluation.synthetic.topology.NetworkTopologyGraphs.{NetworkTopologyGraph, NetworkTopologyGraphNode}
+import karme.graphs.Graphs.{UnlabeledDiGraph, UnlabeledEdge}
 
 class LinearNetworkGeneration(nbNodes: Int) extends NetworkTopologyGeneration {
 
   def generate(): NetworkTopologyGraph = {
-    ???
+    val nodes = makeNodes()
+
+    var graph = UnlabeledDiGraph[NetworkTopologyGraphNode](V = nodes.toSet)
+
+    for ((n1, n2) <- nodes.zip(nodes.tail)) {
+      graph = graph.addEdge(n1, n2)
+    }
+
+    graph
+  }
+
+  private def makeNodes(): Seq[NetworkTopologyGraphNode] = {
+    for (i <- 1 to nbNodes) yield {
+      NetworkTopologyGraphNode(s"v$i")
+    }
   }
 
 }
