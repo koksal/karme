1aae4b1193765d4fa154444939388f16a1417cb4
Ali Sinan Köksal <alisinan@gmail.com>
2017-09-20 18:25:16 -0700

    minor

--- src/main/scala/karme/evaluation/synthetic/Evaluation.scala
+++ src/main/scala/karme/evaluation/synthetic/Evaluation.scala
@@ -1,5 +1,6 @@
 package karme.evaluation.synthetic
 
+import karme.evaluation.synthetic.fungen.RandomFunctionGeneration
 import karme.evaluation.synthetic.topology.LinearNetworkGeneration
 
 object Evaluation {
@@ -9,6 +10,8 @@ object Evaluation {
     val topology = new LinearNetworkGeneration(10).generate()
 
     // 2. Create functions for each node
+    val labelToFun = new RandomFunctionGeneration().generate(topology)
+
     // 3. Pick an initial state
     // 4. Simulate network
     // 5. Optionally alter simulated data (sample, flip bits)
