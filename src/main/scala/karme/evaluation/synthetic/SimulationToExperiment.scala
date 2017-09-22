88cde04fe2a5b092819b9938ce6944bab534248d
Ali Sinan Köksal <alisinan@gmail.com>
2017-09-21 17:29:57 -0700

    finish converting simulation results to experiment and trajectory

--- src/main/scala/karme/evaluation/synthetic/SimulationToExperiment.scala
+++ src/main/scala/karme/evaluation/synthetic/SimulationToExperiment.scala
@@ -2,14 +2,36 @@ package karme.evaluation.synthetic
 
 import karme.CellTrajectories.CellTrajectory
 import karme.Experiments.Experiment
+import karme.Experiments.Measurement
 import karme.synthesis.Transitions.ConcreteBooleanState
+import karme.util.UniqueCounter
+
+import scala.collection.mutable.ListBuffer
 
 object SimulationToExperiment {
+
+  private val counter = new UniqueCounter()
 
   def makeExperimentAndTrajectory(
     stateTimestampPairs: Set[(ConcreteBooleanState, Set[Int])]
   ): (Experiment[Boolean], CellTrajectory) = {
-    ???
+    var measurements = ListBuffer[Measurement[Boolean]]()
+    var measurementIDtoTime = Map[String, Double]()
+
+    for ((state, timestamps) <- stateTimestampPairs) {
+      for (timestamp <- timestamps) {
+        val id = freshMeasurementID()
+        measurements.append(Measurement(id, state))
+        measurementIDtoTime += id -> timestamp
+      }
+    }
+
+    val experiment = Experiment(measurements.toList)
+    (experiment, measurementIDtoTime)
+  }
+
+  private def freshMeasurementID(): String = {
+    "m" + counter.next
   }
 
 }
