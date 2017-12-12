2073a7981645f5084b87ae8954aa9071ebb39e22
Ali Sinan Köksal <alisinan@gmail.com>
2017-12-12 13:28:33 -0800

    minor

--- scripts/run-eval-suite.sh
+++ scripts/run-eval-suite.sh
@@ -8,17 +8,17 @@ else
   OUTFOLDER_BASE=$1
 fi
 
-# ./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE-oracle --partial-order oracle
+# ./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE/oracle --partial-order oracle
 
 for distcomp in ranksum ks
 do
-  for p in 0.1 0.2 0.5
+  for p in 0.01 0.05
   do
-    ./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE-$distcomp-p-$p --partial-order comparison --distribution-comparison $distcomp --distribution-comparison-p-value $p
+    ./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE/$distcomp-p-$p --partial-order comparison --distribution-comparison $distcomp --distribution-comparison-p-value $p
   done
 done
 
 for distcomp in average minimum
 do
-    ./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE-$distcomp --partial-order comparison --distribution-comparison $distcomp
+    ./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE/$distcomp --partial-order comparison --distribution-comparison $distcomp
 done
