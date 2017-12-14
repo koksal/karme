ec18fffb76d80729d479526068b611733e5ca3ed
Ali Sinan Köksal <alisinan@gmail.com>
2017-12-14 11:03:24 -0800

    take extra args in eval suite script

--- scripts/run-eval-suite.sh
+++ scripts/run-eval-suite.sh
@@ -8,17 +8,19 @@ else
   OUTFOLDER_BASE=$1
 fi
 
-# ./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE/oracle --partial-order oracle
+EXTRA_ARGS="--max-expr-depth 3"
+
+./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE/oracle --partial-order oracle $EXTRA_ARGS
 
 for distcomp in ranksum ks
 do
-  for p in 0.01 0.05
+  for p in 0.01 0.05 0.1 0.2 0.5
   do
-    ./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE/$distcomp-p-$p --partial-order comparison --distribution-comparison $distcomp --distribution-comparison-p-value $p
+    ./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE/$distcomp-p-$p --partial-order comparison --distribution-comparison $distcomp --distribution-comparison-p-value $p $EXTRA_ARGS
   done
 done
 
 for distcomp in average minimum
 do
-    ./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE/$distcomp --partial-order comparison --distribution-comparison $distcomp
+    ./scripts/run-synthetic-workflow.sh $OUTFOLDER_BASE/$distcomp --partial-order comparison --distribution-comparison $distcomp $EXTRA_ARGS
 done
