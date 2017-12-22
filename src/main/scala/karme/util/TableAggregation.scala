15f1965441f9edcc419ab4cced2be178bb87e357
Ali Sinan Köksal <alisinan@gmail.com>
2017-12-22 09:55:10 -0800

    generate both median and non-aggregate tables

--- src/main/scala/karme/util/TableAggregation.scala
+++ src/main/scala/karme/util/TableAggregation.scala
@@ -4,35 +4,56 @@ import java.io.File
 
 object TableAggregation {
 
-  val takeMedians = true
-
   def main(args: Array[String]): Unit = {
-    val (outFileName, inFileNames) = (args.head, args.tail)
+    val (outFilenamePrefix, inFileNames) = (args.head, args.tail)
     val data = inFileNames map { n =>
       TSVUtil.readHeadersAndData(new File(n))
     }
 
-    val headers = data.map(_._1)
-    assert(headers.toSet.size == 1)
+    val headerRows = data.map(_._1)
+    assert(headerRows.toSet.size == 1)
+    val headers = headerRows.head
 
-    val tables = inFileNames.zip(data.map(_._2)) map {
-      case (name, table) => {
-        if (takeMedians) {
-          val medianRow = takeMedian(headers.head, table)
-          Seq(medianRow.updated("Run", name))
-        } else {
-          table map { row => row.updated("Run", name)}
-        }
-      }
+    val runIDDataPairs = inFileNames.zip(data.map(_._2))
+
+    val medianData = runIDDataPairs map {
+      case (runID, data) => medianRowForRun(runID, headers, data)
     }
 
-    val outHeader = "Run" :: headers.head
+    val nonAggregateData = runIDDataPairs flatMap {
+      case (runID, data) => nonAggregateRowsForRun(runID, headers, data)
+    }
+
+    val outHeader = "Run" :: headers
 
     TSVUtil.saveTupleMapsWithOrderedHeaders(
       outHeader,
-      tables.toList.flatten,
-      new File(outFileName)
+      medianData,
+      new File(s"$outFilenamePrefix-median.tsv")
     )
+
+    TSVUtil.saveTupleMapsWithOrderedHeaders(
+      outHeader,
+      nonAggregateData,
+      new File(s"$outFilenamePrefix-all.tsv")
+    )
+  }
+
+  def medianRowForRun(
+    runID: String,
+    headers: Seq[String],
+    data: Seq[Map[String, String]]
+  ): Map[String, Any] = {
+    val medianRow = takeMedian(headers, data)
+    medianRow.updated("Run", runID)
+  }
+
+  def nonAggregateRowsForRun(
+    runID: String,
+    headers: Seq[String],
+    data: Seq[Map[String, String]]
+  ): Seq[Map[String, Any]] = {
+    data map { row => row.updated("Run", runID)}
   }
 
   def takeMedian(
