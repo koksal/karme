ea76962a0cd9d6338e0d1248101809f8fe01a5d4
Ali Sinan Köksal <alisinan@gmail.com>
2018-02-25 15:28:50 -0800

    experimental design that compares stable state sets via jaccard index

--- src/main/scala/karme/evaluation/synthetic/expdesign/ExperimentGuide.scala
+++ src/main/scala/karme/evaluation/synthetic/expdesign/ExperimentGuide.scala
@@ -3,14 +3,20 @@ package karme.evaluation.synthetic.expdesign
 import karme.evaluation.PerturbationAnalysis
 import karme.evaluation.synthetic.examples.myeloid.MyeloidModel.KnockoutExperiment
 import karme.synthesis.FunctionTrees.FunExpr
+import karme.synthesis.Transitions.ConcreteBooleanState
 
-trait ExperimentGuide[T] {
+trait ExperimentGuide {
 
-  def evaluateModel(m: Map[String, FunExpr]): T
+  type EvalDomain
 
-  def distance(t1: T, t2: T): Double
+  def evaluateModel(
+    m: Map[String, FunExpr],
+    initialStates: Set[ConcreteBooleanState]
+  ): EvalDomain
 
-  def maxPairwiseDistance(ts: Seq[T]): Double = {
+  def distance(t1: EvalDomain, t2: EvalDomain): Double
+
+  def maxPairwiseDistance(ts: Seq[EvalDomain]): Double = {
     val (t1, t2) = ts.zip(ts) maxBy {
       case (t1, t2) => distance(t1, t2)
     }
@@ -20,13 +26,18 @@ trait ExperimentGuide[T] {
 
   def mostDistinguishingExperiment(
     experiments: Seq[KnockoutExperiment],
-    models: Seq[Map[String, FunExpr]]
+    models: Seq[Map[String, FunExpr]],
+    initialStates: Set[ConcreteBooleanState]
   ): KnockoutExperiment  = {
     experiments maxBy { experiment =>
       val perturbedModels = models map (m =>
         PerturbationAnalysis.knockoutVariable(m, experiment.knockoutVar))
 
-      val evaluationValues = perturbedModels map evaluateModel
+      val perturbedInitialStates = initialStates.map(s =>
+        s.replaceValue(experiment.knockoutVar, false))
+
+      val evaluationValues = perturbedModels map (m =>
+        evaluateModel(m, perturbedInitialStates))
 
       maxPairwiseDistance(evaluationValues)
     }
---
+++ src/main/scala/karme/evaluation/synthetic/expdesign/ExperimentGuideByStableStates.scala
@@ -0,0 +1,26 @@
+package karme.evaluation.synthetic.expdesign
+
+import karme.evaluation.synthetic.FixpointStates
+import karme.synthesis.FunctionTrees.FunExpr
+import karme.synthesis.Transitions.ConcreteBooleanState
+import karme.util.CollectionUtil
+
+object ExperimentGuideByStableStates extends ExperimentGuide {
+
+  type EvalDomain = Set[ConcreteBooleanState]
+
+  def evaluateModel(
+    m: Map[String, FunExpr],
+    initialStates: Set[ConcreteBooleanState]
+  ): Set[ConcreteBooleanState] = {
+    FixpointStates.findSimulationFixpoints(m, initialStates)
+  }
+
+  def distance(
+    t1: Set[ConcreteBooleanState],
+    t2: Set[ConcreteBooleanState]
+  ): Double = {
+    1 - CollectionUtil.jaccardIndex(t1, t2)
+  }
+
+}
